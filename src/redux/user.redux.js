b858c11bdfaa5322fdd9a955a77dba371a9e22a5
sy546846841 <44683670+sy545846841@users.noreply.github.com>
2021-10-23 12:53:05 -0700

    2. Login and register

---
+++ src/redux/user.redux.js
@@ -0,0 +1,89 @@
+import axios from "axios";
+import { getRedirectPath } from "../util";
+const REGISTER_SUCCESS = "REGISTER_SUCCESS";
+const LOGIN_SUCCESS = "LOGIN_SUCCESS";
+const ERROR_MSG = "ERROR_MSG";
+
+const initState = {
+  msg: "",
+  isAuth: false,
+  username: "",
+  password: "",
+  type: "",
+  redirectTo: "",
+};
+
+//Reducers
+export const user = (state = initState, action) => {
+  switch (action.type) {
+    case "REGISTER_SUCCESS":
+      return {
+        ...state,
+        msg: "",
+        isAuth: true,
+        redirectTo: getRedirectPath(action.payload),
+        ...action.payload,
+      };
+    case "LOGIN_SUCCESS":
+      return {
+        ...state,
+        msg: "",
+        isAuth: true,
+        redirectTo: getRedirectPath(action.payload),
+        ...action.payload,
+      };
+    case "ERROR_MSG":
+      return {
+        ...state,
+        isAuth: false,
+        msg: action.msg,
+      };
+    default:
+      return state;
+  }
+};
+
+const registerSuccess = (data) => {
+  return { type: REGISTER_SUCCESS, payload: data };
+};
+const loginSuccess = (data) => {
+  return { type: LOGIN_SUCCESS, payload: data };
+};
+const errorMsg = (msg) => {
+  return { msg, type: ERROR_MSG };
+};
+
+export const register = ({ username, password, repeatPassword, identity }) => {
+  if (!username || !password || !identity) {
+    return errorMsg("You must enter username or password");
+  }
+  if (password !== repeatPassword) {
+    return errorMsg("Password is not the same as repeat password");
+  }
+  return (disptch) => {
+    axios
+      .post("/user/register", { username, password, identity })
+      .then((res) => {
+        if (res.status === 200 && res.data.code === 0) {
+          disptch(registerSuccess({ username, password, identity }));
+        } else {
+          disptch(errorMsg(res.data.msg));
+        }
+      });
+  };
+};
+
+export const login = (username, password) => {
+  if (!username || !password) {
+    return errorMsg("You must enter username and password");
+  }
+  return (disptch) => {
+    axios.post("/user/login", { username, password }).then((res) => {
+      if (res.status === 200 && res.data.code === 0) {
+        disptch(loginSuccess(res.data.data));
+      } else {
+        disptch(errorMsg(res.data.msg));
+      }
+    });
+  };
+};
